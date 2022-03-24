6130bab87ba9977216b10032ddf6cada46d107b5
Manuel Schierenberg <mail@elanum.de>
2022-03-24 16:55:27 +0100

    feat: server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, './src')));
+app.use('/*', (_req, res) => {
+  res.sendFile(path.join(__dirname, './src', 'index.html'));
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
